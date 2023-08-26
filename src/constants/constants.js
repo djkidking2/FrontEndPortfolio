5f8e5efab56e04dc7d91e9d1061f84f37b9b1606
djkidking2 <105659704+djkidking2@users.noreply.github.com>
2023-08-26 02:20:06 -0400

    Add static assets in public folder

---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "Front-end | GPT3",
+    description:
+      "By leveraging React, Node.js, CSS, and Animista, I am capable of developing a complete, responsive application from inception to completion. The App is called GPT-3 and it is a simple Modern UI/UX Website created with React JS. The GPT-3 App showcases seamless interaction and dynamic user experiences, highlighting the fusion of cutting-edge technologies like React, Node.js, CSS, and Animista to deliver a polished and engaging application across all devices.",
+    image: "/images/1.png",
+    tags: ["CSS", "Animista", "React", "Node"],
+    source: "https://github.com/djkidking2",
+    visit: "https://djkidking2.github.io/FrontEndReactApp/",
+    id: 0,
+  },
+  {
+    title: "Front-end | Dining App",
+    description:
+      "In crafting a fully responsive modern restaurant application, I embarked on a design journey that harmoniously blended form and function. By harnessing the power of technologies like React for frontend development and Node.js for backend support, I ensured a seamless user experience across various devices. Through meticulous utilization of CSS, I meticulously stylized each element, creating an aesthetic that resonated with the restaurant's identity while maintaining optimal usability.",
+    image: "/images/2.png",
+    tags: ["React", "JavaScript"],
+    source: "https://github.com/djkidking2",
+    visit: "https://djkidking2.github.io/Restaurant/",
+    id: 1,
+  },
+  {
+    title: "WeTube App",
+    description:
+      "In conceptualizing and designing WeTube, a YouTube-inspired clone, my approach was to replicate the familiar while infusing it with unique features. Through the utilization of cutting-edge technologies such as React and Node.js, I meticulously recreated the core functionalities of video uploading, streaming, and user interactions. The frontend, built with React, not only echoed the aesthetics of YouTube but also introduced a refreshed user interface that enhanced user engagement.",
+    image: "/images/3.jpg",
+    tags: ["React", "WebRTC"],
+    source: "https://github.com/djkidking2",
+    visit: "https://644bc151676c99050038f7df--unrivaled-raindrop-2ac754.netlify.app/",
+    id: 2,
+  },
+  {
+    title: "Figma",
+    description:
+      "Harnessing the versatility of Figma, I adeptly weave together the threads of creativity and functionality to craft captivating front-end designs and prototypes. With a keen eye for detail and a deep understanding of user-centric design principles, I meticulously translate concepts into tangible visual representations. Through Figma's dynamic interface, I orchestrate a symphony of components, colors, and typography, meticulously molding each element to resonate with the project's essence.",
+    image: "/images/4.jpg",
+    tags: ["React", "ChatEngine", "Firebase"],
+    source: "https://google.com",
+    visit: "https://www.figma.com/file/eTzuJU4puElaGivlcxLcAU/Figma-Designs?type=design&node-id=0-1&mode=design&t=EOtJskQAkECHU9x6-0",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2005, text: "Started my journey" },
+  { year: 2016, text: "Worked as a Architectural Designer" },
+  { year: 2019, text: "Worked as a Freelance Designer" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2023, text: "Receiving my Masters in HCI" },
+];
